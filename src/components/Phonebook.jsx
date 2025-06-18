de1640a9af608bee520d8764e2653c993fc3c6e5
Nhshle <Nhshle@users.noreply.github.com>
2025-06-18 23:18:53 +0300

    initial commit

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -38,7 +38,7 @@ const Persons = ({ persons, onDelete }) => {
       {persons.map((person) => (
         <li key={person.id} className="persons">
           {person.name} - {person.number}{" "}
-          <button onClick={() => onDelete(person.id)}>Delete</button>
+          <button onClick={() => onDelete(person.id)} className='delete'>Delete</button>
         </li>
       ))}
     </ol>
